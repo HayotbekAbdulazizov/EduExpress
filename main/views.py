f75c661f7bb66ce5e3bdb741f15c25f7d5bec691
Hayotbek Abdulazizov <hayotbekabdulazizov2005@gmail.com>
2022-03-28 05:45:06 +0600

    telegrambot sender added and index and to university page

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 from django.utils.translation import gettext as _
 from django.contrib import messages
 from .forms import RequestForm
+from .tg_sender import telegram_bot_sendtext
 
 class HomePageView(View):
 	def get(self,request):
@@ -32,7 +33,8 @@ class HomePageView(View):
 			additional = form.cleaned_data['additional']
 			requester = Request.objects.create(name=name, surname=surname, age=age, country=country, program=program, language=language, degree=degree, score=score, phone=phone, additional=additional)
 			print(name, phone, "Thing have came !!!")
-			# messages.success(request, _(f"{requester.name}, Your message was successfully submitted, please wait our call !"))
+			messages.success(request, _(f"{requester.name}, Your message was successfully submitted, please wait our call !"))
+			telegram_bot_sendtext('NEW USER')
 			print("redirecting !!!")
 			return redirect(f'/') 
 		else:
@@ -61,9 +63,11 @@ class UniversityDetailView(View):
 	def post(self, request, slug):
 		form  = RequestForm(request.POST)
 		if form.is_valid():
-			# university_id = form.cleaned_data['univ_id']
-			# print(university_id)
-			univ = University.objects.get(id=1)
+			# university_id = form.cleaned_data['university_id']
+			university_id = request.POST.get('university_id')
+			print(form.cleaned_data)
+			univ = University.objects.get(id=int(university_id))
+			# univ = University.objects.get(id=)
 
 			name = form.cleaned_data['name']
 			surname = form.cleaned_data['surname']
@@ -76,15 +80,15 @@ class UniversityDetailView(View):
 			phone = form.cleaned_data['phone']
 			additional = form.cleaned_data['additional']
 			requester = Request.objects.create(university=univ, name=name, surname=surname, age=age, program=program, language=language, degree=degree, score=score, phone=phone, additional=additional)
-			# requester.country = univ.country.name
+			requester.country = univ.country.name
 			requester.save()
 			print(name, phone, "Thing have came !!!")
 			messages.success(request, _(f"{requester.name}, Your message was successfully submitted, please wait our call !"))
-			print("redirecting !!!")
+			print("Requester Country", requester.country)
+			telegram_bot_sendtext(f'NEW USER {requester.country}',)
 			return redirect(f'/') 
 		else:
 			form = RequestForm
-			print("There is an ERROR !!!")
 			return redirect('/')
 
 
